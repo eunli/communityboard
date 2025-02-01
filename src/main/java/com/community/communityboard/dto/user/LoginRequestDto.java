f14e28e28194fd7504cde305c359ff2ad0268b4c
eunli <eunli99012@gmail.com>
2025-02-01 21:53:58 +0900

    Feat: Add User-related DTOs #3

---
+++ src/main/java/com/community/communityboard/dto/user/LoginRequestDto.java
@@ -0,0 +1,18 @@
+package com.community.communityboard.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequestDto {
+
+  @NotBlank
+  @Email
+  private String email;
+
+  @NotBlank
+  private String password;
+}
